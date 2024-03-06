775604c715c6d6ab85992559035a3ee312b407b2
Leonardo Cornachione <lcornac@gap.com>
2024-03-06 00:06:36 -0300

    added first custom js action

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,5 +1,5 @@
 const core = require('@actions/core');
-const github = require('@actions/gihub');
+const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
